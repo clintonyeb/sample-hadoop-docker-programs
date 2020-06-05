406178f1b0eba4f3859b1dac2d7f59d05ffd29e3
Cinton Yeboah <clintonyeb@gmail.com>
2020-06-05 15:21:09 -0500

    fixing running

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 name=hadoop
 docker stop $name || true && docker rm $name || true
 docker build -t $name .
-docker run --name $name -it $name /etc/bootstrap.sh -bash
+docker run --name $name -it $name /etc/bootstrap.sh #-bash
+
+# copy output to local
+rm -rf ./output
+docker cp $name:/output ./output
